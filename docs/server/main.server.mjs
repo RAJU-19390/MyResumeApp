a38a93c24799da554046fd41428f37f7bb0709e4
RAJU-19390 <www.rajuboda333@gmail.com>
2024-07-22 16:17:23 +0530

    project init

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-LSN4AR2B.mjs";import"./chunk-4H22LZMD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';  // Import CommonModule
+import { SummaryComponent } from './summary/summary.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,  // Add CommonModule here
+    SummaryComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myresumeapp';
+  title: string ='myresumeapp';
+  currentSection: string = 'summary';
+
+  showSection(section: string) {
+    this.currentSection = section;
+  }
 }
